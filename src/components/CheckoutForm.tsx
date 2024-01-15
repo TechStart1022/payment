8f0fcaf5530b8eab2dc1c5266c98ee0fdc6a45b2
whitesnow1022127 <snow1022127@gmail.com>
2024-01-16 05:48:47 +1000

    stripe

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -12,5 +12,27 @@ export const CheckoutForm = () => {
     const elements = useElements();
 
     const [errorMessage, setErrorMessage] = useState<string | undefined>('');
-    
+    const [emailInput, setEmailInput] = useState<String>('');
+
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(elements == null || stripe == null) {
+            return;
+        }
+
+        const { error: submitError } =  await elements.submit();
+        if(submitError?.message) {
+            setErrorMessage(submitError.message)
+            return;
+        }
+        const res = await fetch('https://4b4f7gqeh9.us.aircode.run/payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+    }
+
 }
